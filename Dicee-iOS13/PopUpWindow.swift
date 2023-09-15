992dee9488625c91169d35011b6958ed47c1e436
osmanGuler19 <osman-guler@outlook.com>
2023-09-15 18:07:30 +0300

    first commit

---
+++ Dicee-iOS13/PopUpWindow.swift
@@ -0,0 +1,172 @@
+//
+//  PopupView.swift
+//  Dicee-iOS13
+//
+//  Created by Osman Güler on 6.09.2023.
+//  Copyright © 2023 London App Brewery. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+class PopUpWindow: UIViewController {
+
+    private let popUpWindowView = PopUpWindowView()
+    
+    init(title: String, buttontext: String) {
+        super.init(nibName: nil, bundle: nil)
+        modalTransitionStyle = .crossDissolve
+        modalPresentationStyle = .overFullScreen
+        
+        popUpWindowView.popupTitle.text = title
+        popUpWindowView.popupButton.setTitle(buttontext, for: .normal)
+        popUpWindowView.popupButton.addTarget(self, action: #selector(dismissView), for: .touchUpInside)
+        view = popUpWindowView
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    @objc func dismissView(){
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
+
+private class PopUpWindowView: UIView {
+    
+    let popupView = UIView(frame: CGRect.zero)
+    let popupTitle = UILabel(frame: CGRect.zero)
+    let gifView = UIImageView(frame: CGRect.zero)
+    let popupButton = UIButton(frame: CGRect.zero)
+    
+    let BorderWidth: CGFloat = 2.0
+    
+    init() {
+        super.init(frame: CGRect.zero)
+        // Semi-transparent background
+        backgroundColor = UIColor.black.withAlphaComponent(0.3)
+        
+        // Popup Background
+        popupView.backgroundColor = UIColor.colorFromHex("#646E28")
+        popupView.layer.borderWidth = BorderWidth
+        popupView.layer.masksToBounds = true
+        popupView.layer.borderColor = UIColor.white.cgColor
+        
+        // Popup Title
+        popupTitle.textColor = UIColor.white
+        popupTitle.backgroundColor = UIColor.colorFromHex("#9E1C40")
+        popupTitle.layer.masksToBounds = true
+        popupTitle.adjustsFontSizeToFitWidth = true
+        popupTitle.clipsToBounds = true
+        popupTitle.font = UIFont.systemFont(ofSize: 23.0, weight: .bold)
+        popupTitle.numberOfLines = 1
+        popupTitle.textAlignment = .center
+        
+        // Popup Gif
+        UIImage.fetchGif { [unowned self] result in
+            
+            switch result {
+            case .success(let gif):
+
+                // Update UI using main thread
+                DispatchQueue.main.async {
+                    
+                    // Update collection view content
+                    self.gifView.image = gif
+                }
+                
+            case .failure(let error):
+                print("Request failed with error: \(error)")
+            }
+        }
+        gifView.frame = CGRect(x: 20.0, y: 390.0, width: popupView.frame.size.width - 10, height: 150.0)
+        
+        
+        
+        // Popup Button
+        popupButton.setTitleColor(UIColor.white, for: .normal)
+        popupButton.titleLabel?.font = UIFont.systemFont(ofSize: 23.0, weight: .bold)
+        popupButton.backgroundColor = UIColor.colorFromHex("#9E1C40")
+        
+        popupView.addSubview(popupTitle)
+        popupView.addSubview(gifView)
+        popupView.addSubview(popupButton)
+        
+        // Add the popupView(box) in the PopUpWindowView (semi-transparent background)
+        addSubview(popupView)
+        
+        
+        // PopupView constraints
+        popupView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            popupView.widthAnchor.constraint(equalToConstant: 293),
+            popupView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            popupView.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+            ])
+        
+        // PopupTitle constraints
+        popupTitle.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            popupTitle.leadingAnchor.constraint(equalTo: popupView.leadingAnchor, constant: BorderWidth),
+            popupTitle.trailingAnchor.constraint(equalTo: popupView.trailingAnchor, constant: -BorderWidth),
+            popupTitle.topAnchor.constraint(equalTo: popupView.topAnchor, constant: BorderWidth),
+            popupTitle.heightAnchor.constraint(equalToConstant: 55)
+            ])
+        
+        
+        // PopupText constraints
+        gifView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            gifView.heightAnchor.constraint(greaterThanOrEqualToConstant: 67),
+            gifView.topAnchor.constraint(equalTo: popupTitle.bottomAnchor, constant: 8),
+            gifView.leadingAnchor.constraint(equalTo: popupView.leadingAnchor, constant: 15),
+            gifView.trailingAnchor.constraint(equalTo: popupView.trailingAnchor, constant: -15),
+            gifView.bottomAnchor.constraint(equalTo: popupButton.topAnchor, constant: -8)
+            ])
+
+        
+        // PopupButton constraints
+        popupButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            popupButton.heightAnchor.constraint(equalToConstant: 44),
+            popupButton.leadingAnchor.constraint(equalTo: popupView.leadingAnchor, constant: BorderWidth),
+            popupButton.trailingAnchor.constraint(equalTo: popupView.trailingAnchor, constant: -BorderWidth),
+            popupButton.bottomAnchor.constraint(equalTo: popupView.bottomAnchor, constant: -BorderWidth)
+            ])
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension UIColor {
+    static func rgbColor(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor.init(red: red/255, green: green/255, blue: blue/255, alpha: 1.0)
+    }
+    
+    static func colorFromHex(_ hex: String) -> UIColor {
+        var hexString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        if hexString.hasPrefix("#") {
+            hexString.remove(at: hexString.startIndex)
+        }
+        if hexString.count != 6 {
+            return UIColor.magenta
+        }
+        
+        var rgb: UInt64 = 0
+        Scanner.init(string: hexString).scanHexInt64(&rgb)
+        
+        return UIColor.init(red: CGFloat((rgb & 0xFF0000) >> 16)/255,
+                            green: CGFloat((rgb & 0x00FF00) >> 8)/255,
+                            blue: CGFloat(rgb & 0x0000FF)/255,
+                            alpha: 1.0)
+    }
+    
+}
--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,107 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var player1Label: UILabel!
+    @IBOutlet weak var player2Label: UILabel!
+    
+    @IBOutlet weak var player1Button: UIButton!
+    @IBOutlet weak var player2Button: UIButton!
+    
+    @IBOutlet weak var player1Dice1: UIImageView!
+    @IBOutlet weak var player1Dice2: UIImageView!
+    @IBOutlet weak var player2Dice1: UIImageView!
+    @IBOutlet weak var player2Dice2: UIImageView!
+    
+    @IBOutlet weak var player1ScoreLabel: UILabel!
+    
+    @IBOutlet weak var player2ScoreLabel: UILabel!
+    
+    var dice1: Int?
+    var dice2: Int?
+    var isFirstPlayerDiced = false
+    var isSecondPlayerDiced = false
+    var isGameFinished = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        inverPlayer2Views()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    enum PlayerType{
+        case PLAYER1, PLAYER2
+    }
+    
+    func inverPlayer2Views(){
+        player2Label.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+        player2Button.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+        player2Dice1.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+        player2Dice2.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+    }
+    
+    func diceForButton(diceView1:inout UIImageView,diceView2: inout UIImageView, player: PlayerType){
+        dice1 = Int.random(in: 1...6)
+        dice2 = Int.random(in: 1...6)
+        let total = dice1! + dice2!
+        if(player == PlayerType.PLAYER1){
+            player1ScoreLabel.text = String(total)
+        }else{
+            player2ScoreLabel.text = String(total)
+        }
+        
+        diceView1.image = getDiceView(num: dice1!)
+        diceView2.image = getDiceView(num: dice2!)
+        
+        
+    }
+    
+    func getDiceView(num:Int)->UIImage{
+        switch num{
+        case 1:
+            return UIImage(named: "DiceOne")!
+        case 2:
+            return UIImage(named: "DiceTwo")!
+        case 3:
+            return UIImage(named: "DiceThree")!
+        case 4:
+            return UIImage(named: "DiceFour")!
+        case 5:
+            return UIImage(named: "DiceFive")!
+        default:
+            return UIImage(named: "DiceSix")!
+        }
+        
+    }
+    
+    func checkGameFinished(){
+        if(isFirstPlayerDiced && isSecondPlayerDiced){
+            finishTheGame()
+        }
+    }
+    
+    func finishTheGame(){
+        var popUpWindow: PopUpWindow!
+        popUpWindow = PopUpWindow(title: "Error", buttontext: "OK")
+        self.present(popUpWindow, animated: true, completion: nil)
+    }
+    
+    @IBAction func diceForPlayer1(_ sender: Any) {
+        if(!isFirstPlayerDiced){
+            diceForButton(diceView1: &player1Dice1, diceView2: &player1Dice2, player: PlayerType.PLAYER1)
+            isFirstPlayerDiced = true
+        }
+        checkGameFinished()
+    }
+    
+    @IBAction func diceForPlayer2(_ sender: Any) {
+        if(!isSecondPlayerDiced){
+            diceForButton(diceView1: &player2Dice1, diceView2: &player2Dice2, player: PlayerType.PLAYER2)
+            isSecondPlayerDiced = true
+        }
+        checkGameFinished()
+    }
+    
+    
 }
 
